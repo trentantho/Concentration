7663eec6d318e43f010e0f4ed9dbaf51c8ce0d5e
Trent Morrison <trent.a.morrison@icloud.com>
2024-11-16 00:54:05 -0500

    Allowed card clicking

--- Concentration/Concentration/ContentView.swift
+++ Concentration/Concentration/ContentView.swift
@@ -1,20 +1,79 @@
 import SwiftUI
 import SwiftData
 
-struct ContentView: View {
+struct ContentView: View { // ContentView "Behaves like a" view
   var body: some View { // "some View" means run the code inside and check if it behaves like a view
-    VStack {
-      // ViewBuilder turns these views (Image, Text) into a TupleView
-      Image(systemName: "globe")
-        .imageScale(.large)
-        .foregroundColor(.accentColor)
-      Text("Hello World!")
-        .padding()
+    //Stacks "return" tuples of items
+    HStack {
+      CardView(isFaceUp: true) // setting arguments
+      CardView()
+      CardView(isFaceUp: true)
+      CardView()
     }
-    .imageScale(.large)
-    .foregroundColor(.accentColor)
+    .foregroundColor(.purple)
+    .padding()
   }
 }
+
+struct CardView: View {
+  @State var isFaceUp: Bool = false // default value
+  // @State creates a pointer to isFaceUp
+  var body: some View {
+    ZStack {
+      // ViewBuilder turns these views (Image, Text) into a TupleView
+      let rec = RoundedRectangle(cornerRadius: 15)
+      if isFaceUp {
+        
+        rec // background
+          .foregroundColor(.white)
+        
+        rec // border
+          .strokeBorder(lineWidth: 2)
+        
+        Text("⭐️") // "value" or text for card
+          .fontWeight(.bold)
+          .font(.largeTitle)
+      }
+      else {
+        rec // default color is inside the ContentView HStack
+            // since the color wasn't specified here, it uses
+      }     // the HStack (parent) color.
+
+    }
+    .onTapGesture {
+        isFaceUp = !isFaceUp // pointer doesn't change, but the
+                                // value is changed.
+        /* we could instead use
+         isFaceUp.toggle()
+         to do it without @State in the declaration
+    }
+
+
+
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
