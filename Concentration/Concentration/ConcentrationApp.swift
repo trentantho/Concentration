aeb1e41ece7fed22d77766023c0c6792b02c446a
Trent Morrison <trent.a.morrison@icloud.com>
2024-11-15 20:49:17 -0500

    Created SwiftUI Project

---
+++ Concentration/Concentration/ConcentrationApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct ConcentrationApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
---
+++ Concentration/Concentration/ContentView.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+  var body: some View { // "some View" means run the code inside and check if it behaves like a view
+    VStack {
+      // ViewBuilder turns these views (Image, Text) into a TupleView
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundColor(.accentColor)
+      Text("Hello World!")
+    }
+    .padding()
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView();
+  }
+}
